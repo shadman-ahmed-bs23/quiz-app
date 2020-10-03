1e9933b9c9554a71627b6af0af4991278799f337
shadman-ahmed-bs23 <shadmanahmed874@gmail.com>
2020-10-03 19:12:06 +0600

    Question state added

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,8 +1,62 @@
 import React, { Component, Fragment } from "react";
+import { isCompositeComponentWithType } from "react-dom/test-utils";
 import { Helmet } from "react-helmet";
 
+import questions from "./../questions.json";
+import isEmpty from "./../is-empty.js";
 class Quiz extends React.Component {
+	state = {
+		questions,
+		currentQuestion: {},
+		nextQuestion: {},
+		previousQuestion: {},
+		answer: "",
+		numberOfAnsweredQuestion: 0,
+		currentQuestionIndex: 0,
+		score: 0,
+		correctAnswers: 0,
+		wrongAnswers: 0,
+	};
+	componentDidMount() {
+		const {
+			questions,
+			currentQuestion,
+			nextQuestion,
+			previousQuestion,
+		} = this.state;
+		this.displayQuestions(
+			questions,
+			currentQuestion,
+			nextQuestion,
+			previousQuestion
+		);
+	}
+
+	displayQuestions = (
+		questions = this.state.questions,
+		currentQuestion,
+		nextQuestion,
+		previousQuestion
+	) => {
+		let { currentQuestionIndex } = this.state;
+		if (!isEmpty(this.state.questions)) {
+			questions = this.state.questions;
+			currentQuestion = questions[currentQuestionIndex];
+			nextQuestion = questions[currentQuestionIndex + 1];
+			previousQuestion = questions[currentQuestionIndex - 1];
+			const answer = currentQuestion.answer;
+			this.setState({
+				currentQuestion,
+				nextQuestion,
+				previousQuestion,
+				answer,
+			});
+		}
+	};
 	render() {
+		console.log(questions);
+		const { currentQuestion } = this.state;
+		console.log(currentQuestion);
 		return (
 			<Fragment>
 				<Helmet>
@@ -10,14 +64,14 @@ class Quiz extends React.Component {
 				</Helmet>
 				<div className="container">
 					<div className="questions">
-						<h5>Google was found in what year? </h5>
+						<h5>{currentQuestion.question} </h5>
 						<div className="options-container">
-							<p className="option">1997</p>
-							<p className="option">1998</p>
+							<p className="option">{currentQuestion.optionA}</p>
+							<p className="option">{currentQuestion.optionB}</p>
 						</div>
 						<div className="options-container">
-							<p className="option">1999</p>
-							<p className="option">2000</p>
+							<p className="option">{currentQuestion.optionC}</p>
+							<p className="option">{currentQuestion.optionD}</p>
 						</div>
 
 						<div className="button-container mt-3">
