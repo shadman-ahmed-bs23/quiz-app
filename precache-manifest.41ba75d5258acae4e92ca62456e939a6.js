58704bc610bd9999939c22b9b4a02e3fc6603323
shadman-ahmed-bs23 <shadmanahmed874@gmail.com>
2020-10-04 15:16:59 +0600

    Updates

---
+++ precache-manifest.41ba75d5258acae4e92ca62456e939a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76521ee7d3cf5f85e3bee9e876f79a8c",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "8b0f740b6ab899e277ff",
+    "url": "/quiz-app/static/css/main.c707a3a9.chunk.css"
+  },
+  {
+    "revision": "214e369aa4d36635e349",
+    "url": "/quiz-app/static/js/2.48fd21c4.chunk.js"
+  },
+  {
+    "revision": "c40eb2fb6cd2195a5958ca8c3f5b1a16",
+    "url": "/quiz-app/static/js/2.48fd21c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b0f740b6ab899e277ff",
+    "url": "/quiz-app/static/js/main.876de567.chunk.js"
+  },
+  {
+    "revision": "e15a1632907c25963bb7",
+    "url": "/quiz-app/static/js/runtime-main.4f483d45.js"
+  }
+]);
